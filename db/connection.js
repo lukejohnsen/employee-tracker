4e19acad07a55980675f705f2904f9df22de25fd
lukejohnsen <johnsen.luke@gmail.com>
2022-02-19 11:05:42 -0700

    setting up file structure

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employee_db'
+    }
+);
+
+
+module.exports = connection;
